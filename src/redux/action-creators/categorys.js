30e5b6f8d64d4228567993161f12f8fb9f0166ac
fanrongrong <867538577@exam.com>
2019-10-27 22:56:58 +0800

    react

--- src/redux/action-creators/categorys.js
+++ src/redux/action-creators/categorys.js
@@ -1,4 +1,22 @@
 // 操作所有数据
+
+// 操作所有分类列表数据的action creator
+
 import {
-  
-}
+  RECEIVE_CATEGORYS,
+  ADD_CATEGORY,
+  UPDATE_CATEGORY
+} from '../action-types'
+
+import {
+  reqCategorys,
+  reqAddCategory,
+  reqUpdateCategory
+} from '../../api'
+
+import Category from '../../containers/category'
+
+// 同步action creator
+const receiveCategorys = (categorys) => ({type: RECEIVE_CATEGORYS, data: categorys})
+const addCategory = (category) => ({type: ADD_CATEGORY, data: category})
+const updateCategory = (category) => ({type: UPDATE_CATEGORY, data: category})
