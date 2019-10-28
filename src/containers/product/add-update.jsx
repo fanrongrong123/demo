c535c1f6959305e061146563fd2ccfc4a92b6310
fanrongrong <867538577@exam.com>
2019-10-28 23:19:41 +0800

    react

--- src/containers/product/add-update.jsx
+++ src/containers/product/add-update.jsx
@@ -16,6 +16,7 @@ import LinkButton from '../../components/link-button'
 import {reqAddUpdateProduct} from '../../api'
 import PicturesWall from "./pictures-wall"
 import RichTextEditor from "./rich-text-editor"
+import create from 'antd/lib/icon/IconFont'
 
 const {Item} = Form
 const {Option} = Select
@@ -25,3 +26,144 @@ const {Option} = Select
   state =>({categorys:state.categorys}),
   {getCategorysAsync}
 ) 
+@Form.create()
+class AddUpdate extends Component {
+  // 创建ref容器
+  pwRef = React.createRef()
+  editorRef = React.createRef()
+
+  // 检查价格不低于0
+  validatePrice = (rule,value,callback) => {
+    if (value < 0) {
+      callback('价格不得小于0')
+    }else{
+      callback()
+    }
+  }
+  
+
+  // 添加/更新商品
+  submit =() => {
+    // 表单统一验证
+    this.props.from.validateFields(async(error,valus) =>{
+      if (!error) {
+        values.imgs = this.pwRef.current.getImgs()
+        console.log('imgs', values.imgs)
+
+        // 得到富文本编辑器指定的detail
+        values.detail = this.editorRef.current.getDetail()
+        console.log('detail', values.detail)
+
+        // 更新需要ID数据
+        const result = await reqAddUpdateProduct(valus)
+        if (result.status === 0) {
+          message.success('操作成功')
+          this.props.history.replace('/product')
+        }else{
+          message.error(result.msg)
+        }
+      }
+  })
+}
+componentDidMount(){
+  this.props.getCategorysAsync()
+}
+
+render() {
+  const product = memoryUtils.product
+
+  const title = (
+    <span>
+      <LinkButton onClick={() => this.props.history.goBack()}>
+        <Icon type="arrow-left"></Icon>
+      </LinkButton>
+      <span>商品{product._id ? '修改' : '添加'}</span>
+    </span>
+  )
+  const formLayout = {
+    labelCol: { span: 2 },
+    wrapperCol: { span: 8 },
+  }
+
+  const {categorys, form: {getFieldDecorator}} = this.props
+
+
+  // 渲染到页面
+  return (
+    <Card title={title}>
+      <Form {...formLayout}>
+        <Item label="商品名称">
+          {
+            getFieldDecorator('name', {
+              initialValue: product.name || '',
+              rules: [
+                {required: true, message: '商品名称必须输入'}
+              ]
+            })(
+              <Input placeholder="商品名称"></Input>
+            )
+          }
+          
+        </Item>
+        <Item label="商品描述">
+          {
+            getFieldDecorator('desc', {
+              initialValue: product.desc || '',
+              rules: [
+                {required: true, message: '商品描述必须输入'}
+              ]
+            })(
+              <Input placeholder="商品描述"></Input>
+            )
+          }
+          
+        </Item>
+        <Item label="商品价格">
+          {
+            getFieldDecorator('price', {
+              initialValue: product.price || '',
+              rules: [
+                {required: true, message: '商品价格必须输入'}
+              ]
+            })(
+              <Input type="number" addonAfter="元" placeholder="商品价格"></Input>
+            )
+          }
+          
+        </Item>
+        <Item label="商品分类">
+          {
+            getFieldDecorator('categoryId', {
+              initialValue: product.categoryId || '',
+              rules: [
+                {required: true, message: '商品分类必须输入'}
+              ]
+            })(
+              <Select>
+                <Option value="">未选择</Option>
+                {
+                  categorys.map(c => <Option value={c._id} key={c._id}>{c.name}</Option>)
+                }
+              </Select>
+            )
+          }
+          
+        </Item>
+        <Item label="商品图片" wrapperCol={{ span: 15}}>
+          {/* 内部会将组件对象保存到ref容器对象: current: 组件对象 */}
+          <PicturesWall ref={this.pwRef} imgs={product.imgs}/>
+        </Item>
+
+        <Item label="商品详情" wrapperCol={{ span: 20 }}>
+          <RichTextEditor ref={this.editorRef} detail={product.detail} />
+        </Item>
+        
+        <Item>
+          <Button type="primary" onClick={this.submit}>提交</Button>
+        </Item>
+      </Form>
+    </Card>
+  )
+}
+}
+export default AddUpdate
